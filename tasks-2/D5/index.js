403d701eb6ee8ac6449782957d8a799684377d1a
jean carlos <matos.sd52@gmail.com>
2023-02-28 10:55:11 -0300

    Resolução task 5

---
+++ tasks-2/D5/index.js
@@ -0,0 +1,8 @@
+const palavra = "Estagio job Rotation sao paulo";
+
+let novaString = "";
+
+for (let i = palavra.length - 1; i >= 0; i--) {
+  novaString += palavra[i];
+}
+console.log(novaString);
